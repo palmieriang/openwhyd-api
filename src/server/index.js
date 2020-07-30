493c3182d3bd2670968a652f99c849601e008a6d
Angelo <angelo.palmieri@rufusleonard.com>
2020-07-30 08:00:01 +0100

    server - express-pino-logger

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
 const axios = require('axios');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
 
 app.get('/api/media', async (req, res) => {
   const response = await  axios(`https://openwhyd.org/hot/${req.query.genre}?format=json`)
