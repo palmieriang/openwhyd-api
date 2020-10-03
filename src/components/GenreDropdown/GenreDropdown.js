0f158b6a6e5cc407fff0e4b03388f89f8bdb218c
Angelo <angelo.palmieri@rufusleonard.com>
2020-10-03 08:56:56 +0100

    GenreDropdown

--- src/components/GenreDropdown/GenreDropdown.js
+++ src/components/GenreDropdown/GenreDropdown.js
@@ -5,16 +5,21 @@ import './GenreDropdown.scss';
 const musicGenre = [
     { label: 'All', value: 'all', key: 'all' },
     { label: 'Blues', value: 'blues', key: 'blues' },
+    { label: 'Classical', value: 'classical', key: 'classical' },
     { label: 'Electro', value: 'electro', key: 'electro' },
     { label: 'Folk', value: 'folk', key: 'folk' },
     { label: 'Hip Hop', value: 'hip-hop', key: 'hip-hop' },
     { label: 'Indie', value: 'indie', key: 'indie' },
+    { label: 'Jazz', value: 'jazz', key: 'jazz' },
+    { label: 'Latin', value: 'latin', key: 'latin' },
     { label: 'Metal', value: 'metal', key: 'metal' },
     { label: 'Pop', value: 'pop', key: 'pop' },
     { label: 'Punk', value: 'punk', key: 'punk' },
+    { label: 'Reggae', value: 'reggae', key: 'reggae' },
     { label: 'R&B', value: 'rnb', key: 'rnb' },
     { label: 'Rock', value: 'rock', key: 'rock' },
     { label: 'Soul', value: 'soul', key: 'soul' },
+    { label: 'World', value: 'world', key: 'world' },
 ];
 
 const GenreDropdown = ({ changeMusicGenre, initialGenre }) => {
